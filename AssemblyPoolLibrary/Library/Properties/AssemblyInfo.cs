aecd1c5ea46353fc73ccee8a85af6287c2aeec08
Krasimir Stefanov <kraskoo87@gmail.com>
2017-04-12 13:03:04 +0300

    Fix description

--- AssemblyPoolLibrary/Library/Properties/AssemblyInfo.cs
+++ AssemblyPoolLibrary/Library/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AssemblyPoolLibrary")]
-[assembly: AssemblyDescription("Contains three classes with dependency between them, ProjectNameContainer is a public non-static class with")]
+[assembly: AssemblyDescription("Contains three classes with dependency between them, ProjectNameContainer is a public non-static")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Library")]
